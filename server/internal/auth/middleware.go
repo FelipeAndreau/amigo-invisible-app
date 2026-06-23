92f03922fe92c7a33cb2505731bce1a5c1528d28
Santiago Gonzalez D'Angelo <santiago.gonzalez@alu.frlp.utn.edu.ar>
2026-06-23 18:49:56 +0000

    feat(auth): add JWT authentication

---
+++ server/internal/auth/middleware.go
@@ -0,0 +1,39 @@
+package auth
+
+import (
+	"amigo-invisible-server/internal/platform/auth"
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authHeader := c.GetHeader("Authorization")
+		if authHeader == "" {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization header is required"})
+			c.Abort()
+			return
+		}
+
+		parts := strings.Split(authHeader, " ")
+		if len(parts) != 2 || parts[0] != "Bearer" {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization header format must be Bearer {token}"})
+			c.Abort()
+			return
+		}
+
+		claims, err := auth.ValidateToken(parts[1])
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid or expired token"})
+			c.Abort()
+			return
+		}
+
+		// Pasar el UserID al contexto para usarlo en los handlers
+		c.Set("userID", claims.UserID)
+		c.Set("userEmail", claims.Email)
+		c.Next()
+	}
+}
